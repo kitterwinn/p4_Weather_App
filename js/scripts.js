bf4ed4deada057892e60b85c319cbdbfa5eaf03e
Cross <kcross6@vcd-ch204-10.eastern.ewu.edu>
2017-06-08 13:43:02 -0700

    simpleWeather Plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-// Scripts
+// Note: Plugin has been updated. Use version 3.1
+// https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
+// Refer: http://simpleweatherjs.com
+
+$.simpleWeather({
+    location: 99001,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+      $('img').attr('src', weather.image);
+      
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  });
+
+
